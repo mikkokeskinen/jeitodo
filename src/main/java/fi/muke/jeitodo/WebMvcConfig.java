ddeea46277f55580078d3bb65d0eb6c44849efa1
Mikko Keskinen <keso@iki.fi>
2017-09-18 21:18:36 +0300

    Enable Security and Layout dialects

--- src/main/java/fi/muke/jeitodo/WebMvcConfig.java
+++ src/main/java/fi/muke/jeitodo/WebMvcConfig.java
@@ -1,4 +1,20 @@
 package fi.muke.jeitodo;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
 public class WebMvcConfig {
+    @Bean
+    public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver, SpringSecurityDialect sec) {
+        final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(sec);
+        return templateEngine;
+    }
 }
